d701aa94e79a856d6bcd8b2586fe0c52b949eb04
shahadot99999 <shahadot99999@gmail.com>
2026-08-05 17:13:03 +0600

    Creating Login Form With ShadCn

---
+++ app/(authGroup)/layout.tsx
@@ -0,0 +1,14 @@
+
+
+const AuthGroupLayout = async (
+    { children }: { children: React.ReactNode }) => {
+
+        
+  return <div className="max-w-7xl mx-auto">
+   
+    {children}
+  
+  </div>;
+};
+
+export default AuthGroupLayout;
