4bbceb162a848fe36914e9d12c3af8c584f625f7
shahadot99999 <shahadot99999@gmail.com>
2026-08-05 13:05:55 +0600

    Setting Up The Folder Structure Of The Project

---
+++ app/(authGroup)/_components/LoginButton.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+
+const LoginButton = () => {
+
+  return (
+    <Button>
+      Login
+      
+    </Button>
+  );
+}
+
+export default LoginButton
